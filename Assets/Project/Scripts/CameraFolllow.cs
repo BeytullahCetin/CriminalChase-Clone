4e5fb2198791f45f0a283b13dffb9aa47b330158
Beytullah Çetin <44231507+BeytullahCetin@users.noreply.github.com>
2022-10-22 13:50:38 +0300

    Add basic player movement

---
+++ Assets/Project/Scripts/CameraFolllow.cs
@@ -0,0 +1,20 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CameraFolllow : MonoBehaviour
+{
+    [SerializeField] Transform objectToFollow;
+    [SerializeField] Vector3 cameraOffset;
+    [Range(0f,1f)]
+    [SerializeField] float t = .5f;
+
+    private void Start() {
+        cameraOffset = transform.position;
+    }
+
+    void LateUpdate()
+    {
+        transform.position = Vector3.Lerp(transform.position, objectToFollow.position + cameraOffset, t);
+    }
+}
---
+++ Assets/Project/Scripts/PlayerMovement.cs
@@ -0,0 +1,39 @@
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.InputSystem;
+using UnityEngine.UI;
+
+public class PlayerMovement : MonoBehaviour
+{
+    [SerializeField] private CharacterController characterController;
+    [Range(1f,10f)]
+    [SerializeField] private float moveSpeed = 5f;
+    [Range(5f,20f)]
+    [SerializeField] private float rotateSpeed = 10f;
+
+    private Vector2 movementInput;
+    private Vector3 moveVector;
+
+    private void Update()
+    {
+        Movement();
+    }
+
+    private void Movement()
+    {
+        transform.forward = Vector3.Lerp(transform.forward, moveVector, rotateSpeed * Time.deltaTime);
+        characterController.Move(moveVector * moveSpeed * Time.deltaTime);
+    }
+
+    public void GetMovementInput(InputAction.CallbackContext ctx)
+    {
+        movementInput = ctx.ReadValue<Vector2>();
+
+        moveVector.x = movementInput.x;
+        moveVector.z = movementInput.y;
+
+        moveVector.Normalize();
+    }
+}
